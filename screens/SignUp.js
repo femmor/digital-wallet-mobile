a0995d2fb04de326cd92c8217085687bdac8b117
Emmanuel Egomson <fegomson@gmail.com>
2021-01-29 10:21:31 -0600

    finished the renderForm, created renderButton and implemented the showPassword state

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     View, 
     Text,
@@ -19,6 +19,10 @@ import { Colors } from 'react-native/Libraries/NewAppScreen'
 import {icons, images, COLORS, SIZES, FONTS} from "../constants"
 
 const SignUp = () => {
+
+    const [showPassword, setShowPassword] = useState(false)
+
+
     // renderHeader
     const renderHeader = () => (
         <TouchableOpacity
@@ -69,6 +73,202 @@ const SignUp = () => {
         </View>
     )
 
+    // renderForm
+    const renderForm = () => (
+        <View
+            style={{
+                marginTop: SIZES.padding * 3,
+                marginHorizontal: SIZES.padding * 3
+            }}
+        >
+            {/* Full Name */}
+            <View
+                style={{
+                    marginTop: SIZES.padding * 3
+                }}
+            >
+                <Text
+                    style={{
+                        color: COLORS.lightGreen,
+                        ...FONTS.body3
+                    }}
+                >Full Name</Text>
+                <TextInput
+                    style={{
+                        marginVertical: SIZES.padding,
+                        borderBottomColor: COLORS.white,
+                        borderBottomWidth: 1,
+                        height: 40,
+                        color: COLORS.white,
+                        ...FONTS.body3
+                    }}
+                    placeholder="Enter Full Name"
+                    placeholderTextColor={COLORS.lightGray}
+                    keyboardAppearance='dark'
+                    selectionColor={COLORS.white}
+                />
+            </View>
+            
+            {/* Phone Code */}
+            <View
+                style={{
+                    marginTop: SIZES.padding * 2
+                }}
+            >
+                <Text
+                    style={{
+                        color: COLORS.lightGreen,
+                        ...FONTS.body3
+                    }}
+                >Phone Number</Text>
+                <View
+                    style={{
+                        flexDirection: 'row'
+                    }}
+                >
+                    {/* Country Code */}
+                    <TouchableOpacity
+                        style={{
+                            width: 100,
+                            height: 50,
+                            marginHorizontal: 5,
+                            borderBottomColor: COLORS.white,
+                            borderBottomWidth: 1,
+                            flexDirection: 'row',
+                            ...FONTS.body2
+                        }}
+                        onPress={() => console.log('Show modal')}
+                    >
+                        <View
+                            style={{
+                                justifyContent: 'center'
+                            }}
+                        >
+                            <Image source={icons.down} style={{
+                                width: 10,
+                                height: 10,
+                                tintColor: COLORS.white
+                            }}/>
+                        </View>
+                        <View
+                            style={{
+                                justifyContent: 'center',
+                                marginLeft: 5
+                            }}
+                        >
+                            <Image source={images.usFlag} style={{
+                                width: 30,
+                                height: 30,
+                                resizeMode: 'contain'
+                            }}/>
+                        </View>
+                        <View
+                            style={{
+                                justifyContent: 'center',
+                                marginLeft: 5
+                            }}
+                        >
+                            <Text style={{
+                                color: COLORS.white,
+                                ...FONTS.body3
+                            }}>US +1</Text>
+                        </View>
+                    </TouchableOpacity>
+
+                    {/* Phone Number */}
+                <TextInput
+                    style={{
+                        flex: 1,
+                        marginVertical: SIZES.padding,
+                        borderBottomColor: COLORS.white,
+                        borderBottomWidth: 1,
+                        height: 40,
+                        color: COLORS.white,
+                        ...FONTS.body3
+                    }}
+                    placeholder='Enter Phone Number'
+                    placeholderTextColor={COLORS.white}
+                    selectionColor={COLORS.white}
+                    keyboardAppearance='dark'
+                    keyboardType='phone-pad'
+                />
+                </View>
+            </View>
+            
+            {/* Password */}
+            <View style={{
+                marginTop: SIZES.padding * 2
+            }}>
+                <Text
+                    style={{
+                        color: COLORS.lightGreen,
+                        ...FONTS.body3
+                    }}
+                >Password</Text>
+                <TextInput
+                    style={{
+                        flex: 1,
+                        marginVertical: SIZES.padding,
+                        borderBottomColor: COLORS.white,
+                        borderBottomWidth: 1,
+                        height: 40,
+                        color: COLORS.white,
+                        ...FONTS.body3
+                    }}
+                    placeholder='Enter Password'
+                    placeholderTextColor={COLORS.white}
+                    selectionColor={COLORS.white}
+                    secureTextEntry={!showPassword}
+                    keyboardAppearance='dark'
+                />
+                <TouchableOpacity
+                    style={{
+                        position: 'absolute',
+                        right: 0,
+                        bottom: 10,
+                        height: 30,
+                        width: 30
+                    }}
+                    onPress={() => setShowPassword(!showPassword)}
+                >
+                    <Image
+                        source={icons.eye}
+                        style={{
+                            height: 20,
+                            width: 20,
+                            tintColor: COLORS.white
+                        }}
+                    />
+                </TouchableOpacity>
+            </View>
+            
+        </View>
+    )
+
+    // renderButton
+    const renderButton = () => (
+        <View style={{
+            margin: SIZES.padding * 3
+        }}>
+            <TouchableOpacity
+                style={{
+                    height: 60,
+                    backgroundColor: COLORS.black,
+                    borderRadius: SIZES.radius,
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+                onPress={() => console.log('Navigate to Home')}
+            >
+                <Text
+                    style={{
+                        color: COLORS.white,
+                        ...FONTS.body3
+                    }}
+                >Continue</Text>
+            </TouchableOpacity>
+        </View>
+    )
 
     // SignUp Screen View
     return (
@@ -87,6 +287,8 @@ const SignUp = () => {
                 <ScrollView>
                     {renderHeader()}
                     {renderLogo()}
+                    {renderForm()}
+                    {renderButton()}
                 </ScrollView>
             </LinearGradient>
         </KeyboardAvoidingView>
