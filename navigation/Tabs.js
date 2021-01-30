06f83d6115b42b4ddc56a32249c60e134c0226a4
Emmanuel Egomson <fegomson@gmail.com>
2021-01-30 08:54:00 -0600

    complete bottom navigation

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -80,6 +80,30 @@ const TabBarCustomButton = ({ accessibilityLabel, accessibilityState, children, 
     }
 }
 
+
+const CustomTabBar = (props) => {
+    if (isIphoneX()) {
+        return (
+            <View>
+                <View style={{
+                    position:"absolute",
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    height: 30,
+                    backgroundColor: COLORS.white
+                }}></View>
+                <BottomTabBar {...props.props}/>
+            </View>
+        )
+    } else {
+        return (
+            <BottomTabBar {...props.props}/>
+        )
+    }
+
+}
+
 const Tabs = () => {
 
     return (
@@ -95,6 +119,11 @@ const Tabs = () => {
                     elevation: 0
                 }
             }}
+            tabBar={(props) => (
+                <CustomTabBar
+                    props={props}
+                />
+            )}
         >
             <Tab.Screen
                 name="Home"
